dd247d8e5ddaad38d3e97d2780f0ca06281997b4
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2018-09-03 19:07:31 -0700

    [c#] Started .NET Core implementation

---
+++ c#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LanguageBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
