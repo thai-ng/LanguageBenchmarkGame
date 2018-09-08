af664905a04b1e38fb0bfc2fe7c93c4b36f9d992
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2018-09-07 22:24:25 -0700

    [tests] Add 'verify' operation

--- runner.py
+++ runner.py
@@ -12,6 +12,7 @@ def help(args = None):
     print(" 'help' for this text")
     print(" 'init <language name>' to start implementing a new <language>")
     print(" 'run <language> [space-separated arguments]' to run a given <language> implementation with a set of [arguments]")
+    print(" 'verify <language> [space-separated arguments]' to check a given <language> against the reference")
     print(" 'benchmark <repetitions> <language> [space-separated arguments]' run an implementation and take an average time")
     print(" 'compare <comma-separated list of languages> <repetitions> [space-separated arguments]' run some implementations and compare the average time")
 # end help
@@ -24,6 +25,9 @@ def import_from(module, name):
 
 def init(args):
     dir_name = args[0]
+    if ',' in dir_name:
+        raise ValueError("Cannot have ',' in the implementation name.")
+
     resolved_path = os.path.join(os.getcwd(), dir_name)
     if not os.path.exists(resolved_path):
         os.mkdir(dir_name)
@@ -60,6 +64,9 @@ if __name__=="__main__":
 #end init
 
 def run(args):
+    # save the previous working directory
+    working_dir = os.getcwd()
+    
     dir_name = args[0]
     if not os.path.exists(os.path.join(os.getcwd(), dir_name) ) :
         raise NameError("Could not find directory '{}'".format(dir_name))
@@ -71,7 +78,7 @@ def run(args):
     print("")
 
     os.chdir(dir_name)
-    exec(open(os.path.join('.','run.py')).read())
+    #exec(open(os.path.join('.','run.py')).read())
 
     module_name = dir_name+'.run'
     setup = import_from(module_name, 'setup')
@@ -82,12 +89,53 @@ def run(args):
     build()
     
     run(sub_args)
+    
+    # restore working directory
+    os.chdir(working_dir)
 #end run
+
+def verify(args):
+    from difflib import context_diff
+
+    baseline = 'python'
+    comparison = args[0]
+    output_file = 'reference.patch'
+
+    run([baseline] + args[1:])
+    run(args)
+    
+    diff = None
+    with open(os.path.join(baseline, output_file), 'r') as baseline_output:
+        with open(os.path.join(comparison, output_file), 'r') as comparison_output:
+            diff = list(context_diff(baseline_output.readlines(), comparison_output.readlines()))
+
+    # This lambda is a shorthand for when verification might fail
+    verification_fails_with = lambda entries,pattern : len(entries) > 2 or not all(lines.startswith(pattern) for lines in entries)
+    verification_failure = False
+
+    conflicting_lines = [x for x in diff if x[0]=='!']
+    if verification_fails_with(conflicting_lines, '! # Results'):
+        print("'{}' verification failure: more than one conflicting line found (other than file header)".format(comparison))
+        verification_failure = True
+    
+    additional_lines = [x for x in diff if x[0]=='+']
+    if verification_fails_with(additional_lines, '+ {}'.format(os.linesep)):
+        print("'{}' verification failure: additional lines of difference are not newlines ({})".format(comparison, os.linesep))
+        verification_failure = True
+
+    if verification_failure:
+        raise NotImplementedError("Check the implementation of {} against {}".format(comparison, baseline))
+
+    print()
+    print("'{}' meets the implementation criteria".format(comparison))
+#end verify
 
 def benchmark(args):
     import time
     
+    # save the current working dir
     working_dir = os.getcwd()
+
     repetitions = int(args[0])
     dir_name = args[1]
     sub_args = args[2:]
@@ -116,6 +164,7 @@ def benchmark(args):
     print("{} repetitions - average run time: {} seconds".format(repetitions, average))
     print("")
     
+    # restore working dir
     os.chdir(working_dir)
     return average
 #end benchmark
